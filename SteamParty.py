6330d43f995a1600ed426c9a1ad0ebd9eaf7dedc
Frantisek Sumsal <frantisek@sumsal.cz>
2018-05-30 19:46:34 +0200

    Properly initialize colorama on windows

--- SteamParty.py
+++ SteamParty.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import colorama
 import json
 import logging
 import pickle
@@ -186,6 +187,8 @@ class SteamGrabber:
         return set(games)
 
 if __name__ == "__main__":
+    colorama.init()
+
     # Setup logging
     logging.basicConfig(level=logging.INFO,
             format="%(asctime)-14s [%(module)s/%(funcName)s] %(levelname)s: %(message)s")
